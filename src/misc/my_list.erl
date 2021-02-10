74f80f1a2c0107ada39f0a072f25bd2609273c51
vnacu <nvswrk@gmail.com>
2021-02-10 14:18:27 +0200

    Add occurrence, index_of and first_non_repeating_element functions

--- src/misc/my_list.erl
+++ src/misc/my_list.erl
@@ -1,6 +1,6 @@
 -module(my_list).
 
--export([distinct/1, member_of/2, reverse/1, sort/1]).
+-export([distinct/1, member_of/2, reverse/1, sort/1, index_of/2, occurrence/2, first_non_repeating_element/1]).
 
 %% distinct values api
 
@@ -56,4 +56,46 @@ sort([H | T], Current, Temp, Leftover) ->
 
 sort([], Current, Temp, []) ->
   [Current | Temp].
+
+%% element occurrence api
+
+occurrence(Element, List) ->
+  occurrence(Element, List, 0).
+
+occurrence(Element, [H | T], Counter) ->
+  if
+    Element =:= H -> occurrence(Element, T, Counter + 1);
+    Element =/= H -> occurrence(Element, T, Counter)
+  end;
+
+occurrence(_, [], Counter) ->
+  Counter.
+
+%% index of api
+
+index_of(Element, List) ->
+  index_of(Element, List, 1).
+
+index_of(Element, [H | T], Index) ->
+  case Element =:= H of
+    true -> Index;
+    false -> index_of(Element, T, Index + 1)
+  end;
+
+index_of(_, [], _) ->
+  list_is_empty.
+
+%% first non repeating element api
+
+first_non_repeating_element(List) ->
+  first_non_repeating_element(List, List).
+
+first_non_repeating_element([H | T], Initial) ->
+  case occurrence(H, Initial) of
+    1 -> {H, index_of(H, Initial)};
+    _ -> first_non_repeating_element(T, Initial)
+  end;
+
+first_non_repeating_element([], []) ->
+  list_is_empty.
 
