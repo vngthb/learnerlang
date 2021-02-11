5bbc0e854e6cb290dc4256b5ca3de0b999c96ead
vnacu <nvswrk@gmail.com>
2021-02-11 15:11:45 +0200

    Simplify rps logic

--- src/rps/rps.erl
+++ src/rps/rps.erl
@@ -1,39 +1,27 @@
 -module(rps).
 
--export([rps/2]).
+%%-include_lib("hmm").
 
-rps(P1_Input, P2_Input) when length(P1_Input) =:= length(P2_Input) ->
+-export([play/2]).
+
+play(P1_Input, P2_Input) when length(P1_Input) =:= length(P2_Input) ->
   deduce(P1_Input, P2_Input, 0, 0, 0);
 
-rps(P1_Input, P2_Input) when length(P1_Input) =/= length(P2_Input) ->
-  input_lists_must_have_same_size.
+play(P1_Input, P2_Input) when length(P1_Input) =/= length(P2_Input) ->
+  size_mismatch.
 
-deduce([paper | T1], [rock | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins + 1, P2_Wins, Draws);
+deduce(_, _, P1_Wins, P2_Wins, Ties) when P1_Wins == 3; P2_Wins == 3 ->
+  {P1_Wins, P2_Wins, Ties};
 
-deduce([rock | T1], [scissors | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins + 1, P2_Wins, Draws);
+deduce([H1 | T1], [H2 | T2], P1_Wins, P2_Wins, Ties) ->
+  case {H1, H2} of
+    {Value, Value} -> deduce(T1, T2, P1_Wins, P2_Wins, Ties + 1);
+    {Value1, Value2} when
+      (Value1 == paper andalso Value2 == rock);
+      (Value1 == rock andalso Value2 == scissors);
+      (Value1 == scissors andalso Value2 == paper) -> deduce(T1, T2, P1_Wins + 1, P2_Wins, Ties);
+    {_, _} -> deduce(T1, T2, P1_Wins, P2_Wins + 1, Ties)
+  end;
 
-deduce([scissors | T1], [paper | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins + 1, P2_Wins, Draws);
-
-deduce([rock | T1], [paper | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins, P2_Wins + 1, Draws);
-
-deduce([paper | T1], [scissors | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins, P2_Wins + 1, Draws);
-
-deduce([scissors | T1], [rock | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins, P2_Wins + 1, Draws);
-
-deduce([Value | T1], [Value | T2], P1_Wins, P2_Wins, Draws) ->
-  deduce(T1, T2, P1_Wins, P2_Wins, Draws + 1);
-
-deduce(_, _, P1_Wins, _, _) when P1_Wins == 3 ->
-  io:format("Player 1 wins by scoring ~B points!~n", [P1_Wins]);
-
-deduce(_, _, _, P2_Wins, _) when P2_Wins == 3 ->
-  io:format("Player 2 wins by scoring ~B points!~n", [P2_Wins]);
-
-deduce([], [], P1_Wins, P2_Wins, Draws) ->
-  {player_1_wins, P1_Wins, player_2_wins, P2_Wins, Draws}.
+deduce([], [], P1_Wins, P2_Wins, Ties) ->
+  {P1_Wins, P2_Wins, Ties}.
